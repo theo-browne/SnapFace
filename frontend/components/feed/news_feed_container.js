de7843f37d2d9d976cc408f4f390f1772a538572
Theo <theodorejbrowne@gmail.com>
2019-09-16 11:17:30 -0700

    fixed user show page positioning issue

--- frontend/components/feed/news_feed_container.js
+++ frontend/components/feed/news_feed_container.js
@@ -9,7 +9,7 @@ const mSTP = state => ({
     posts: Object.values(state.entities.posts).reverse(),
     currentUser: state.session,
     maxPages: state.pagination.newsFeedPageLength,
-    users: Object.values(state.entities.users).reverse()
+    users: Object.values(state.entities.users).filter(user => user.mutualFriends >= 0).reverse()
 })
 
 const mDTP = dispatch => ({
