3310e75cb9c194cced81a05118b912b04e2b1101
Theo <theodorejbrowne@gmail.com>
2019-09-29 09:56:38 -0700

    restyled suggested friends

--- frontend/components/home/suggested_friend_item.jsx
+++ frontend/components/home/suggested_friend_item.jsx
@@ -17,20 +17,28 @@ export default class SuggestedFriendsItem extends React.Component {
     }
 
     render(){
-        let button = !this.state.added ? (<button onClick={this.handleClick}>Add Friend</button>) :
+        let button = !this.state.added ? (<button className="unadded-friend" onClick={this.handleClick}>Add Friend</button>) :
         (<button className="added-friend">Added</button>)
         return(
         <div className="suggested-friends">
             <div className="suggested-details">
-                <Link to={`/users/${this.props.user.id}`}>
+               
                     <div className="suggested-img">
+                            <Link to={`/users/${this.props.user.id}`}>
                 <img src={this.props.user.profileUrl} alt=""/>
+                        </Link>
                 </div>
+                <div>
+                            <Link to={`/users/${this.props.user.id}`}>
                 <li>{this.props.user.name}</li>
-                    </Link>
+                                </Link>
+                  
+                    <li className="mutual-friends">Mutual Friends: {this.props.user.mutualFriends}</li>
+                    </div>
+                    {button}
             </div>
-                <li className="mutual-friends">Mutual Friends: {this.props.user.mutualFriends}</li>
-            {button}
+               
+            
             {/* <button onClick={() => this.props.createFriendship({ status: "CONFIRMED", friend_id: this.props.user.id })}>Add Friend</button> */}
         </div>
         )
