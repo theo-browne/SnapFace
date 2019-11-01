50ad112572cc6c131e454a8050774dc5c3a3118e
Theo <theodorejbrowne@gmail.com>
2019-11-01 14:05:16 -0700

    signup documentation

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -13,6 +13,12 @@ export default class SignUp extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this)
         this.demoLogin = this.demoLogin.bind(this)
     }
+
+/**
+* Creates an account for a demo user. Random email string used to limit the probablity of the 
+same email being used more than once
+* @param {event} e - The submit event triggered by clicking the Demo Login button.
+*/
     demoLogin(e){
         e.preventDefault()
         let email = String(Math.random())
@@ -27,12 +33,18 @@ export default class SignUp extends React.Component {
         
     }
 
+/**
+* Handles input on the Sign Up form
+*/
     handleInput(str) {
         return (e) => {
             this.setState({ [str]: e.target.value })
         }
     }
-
+/**
+* Submits the create account request. Name is stored in lower case as this is how it is stored in the database.
+* @param {event} e - The submit event triggered by clicking the Sign Up button.
+*/
     handleSubmit(e) {
         e.preventDefault()
         const user = {
@@ -43,6 +55,9 @@ export default class SignUp extends React.Component {
         this.props.createUser(user)
     }
 
+/**
+* Renders the signup form, splash page, and any errors caused by an invalid signup attempt
+*/
     render() {
         let nameId = this.props.errors["Name"] ? "error" : "input"
         let emailId = this.props.errors["Email"] ? "error" : "input"
@@ -53,42 +68,30 @@ export default class SignUp extends React.Component {
   
         return (
             <div className="splash-sign-up">
-                
                 <div className="splash-sign-up-content">
                 <div className="splash-sign-up-left" >
                     <img className="icon" src="https://image.flaticon.com/icons/svg/864/864685.svg" alt=""/>
                         <img src="https://images.unsplash.com/photo-1471306224500-6d0d218be372?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80" alt=""/>
-                    {/* <p>Welcome to SnapFace! The unofficial and unaffiliated social media site of New England Patriots' Head Coach Bill Belichick</p> */}
                 </div>
                 <div className="auth-forms">
                     <SignInContainer />
                     <h2 className="signup-header">Create a New Account</h2>
-                    {/* {this.props.errors.map((error, idx) => <li key={idx} className="signup-error">{error}</li>)} */}
             <form className="auth-form">
-                
                     <div className="sign-in-names" >
                                 <input type="text" className={nameId} value={this.state.firstName} placeholder="First Name" autoComplete="name" onChange={this.handleInput("firstName")} />
                                 <input type="text" className={nameId} value={this.state.lastName} placeholder="Last Name" autoComplete="family-name" onChange={this.handleInput("lastName")} />
                             <li>{nameError}</li>
                     </div>
-                    {/* <br /> */}
-                
                             <input type="text" className={emailId} value={this.state.email} placeholder="Email" autoComplete="email" onChange={this.handleInput("email")} />
                             <li>{emailError}</li>
-                    {/* <br /> */}
-                
                             <input type="password" className={passwordId} value={this.state.password} autoComplete="new-password" placeholder="New Password" onChange={this.handleInput("password")} />
                             <li>{passwordError}</li>
-                   {/* <br/> */}
                    <div className="sign-up-button">
                 <button onClick={this.handleSubmit}>Sign Up</button>
                 <button id="demo-login" onClick={this.demoLogin}> Demo Login</button>
                  </div>
             </form>
             </div>
-                {/* <div className="splash-sign-up-right" >
-
-                </div> */}
                 </div>
             </div>
         )
