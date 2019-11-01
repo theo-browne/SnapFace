ad37bf10260c77859054f96ae2b1efb4440cf08f
Theo <theodorejbrowne@gmail.com>
2019-11-01 16:00:54 -0700

    messaging documentation

--- frontend/components/messaging/message_panel.jsx
+++ frontend/components/messaging/message_panel.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import MessageIndex from './message_index'
-import MessageForm from './message_form'
 import MessageIndexContainer from './message_index_container'
 
 export default class MessagePanel extends React.Component{
@@ -14,8 +12,6 @@ export default class MessagePanel extends React.Component{
             <div className="message-panel">
                 <div className="message-panel-content">
                 <MessageIndexContainer friendship={this.props.friendship} />
-                {/* <MessageIndex friendship={this.props.friendship} messages={this.props.messages}fetchMessages={this.props.fetchMessages}/> */}
-           
                 </div>
             </div>
         )
--- frontend/components/messaging/messages.jsx
+++ frontend/components/messaging/messages.jsx
@@ -8,28 +8,38 @@ export default class Messages extends React.Component{
         this.shown = false
         this.handleClick = this.handleClick.bind(this)
     }
+
+
     componentDidMount(){
         this.props.fetchFriendships().then(() => {
             this.props.friendships.forEach(friendship => {
-                App.cable.subscriptions.create(
+                this.createSubscription(friendship)
+            })
+        })
+    }
+/**
+* Creates the subscription to each messaging channel the user is a part of.
+* @param {friendship} friendship - The friendship object returned from the API. Chat room id is attached to friendship
+*/
+    createSubscription(friendship){
+        App.cable.subscriptions.create(
                     { channel: `RoomChannel`, room_id: friendship.room_id },
-                    // { channel: `RoomChannel_${friendship.room_id}`},
                     {
                         received: data => {
                             if (data.message.roomId === friendship.room_id) {
                                 this.props.receiveMessage(data.message)
-                                
-                                // setTimeout(this.props.updateUnread, 1000)
                                     this.props.updateUnread();
-                            
                             }
                         }
                     }
                 )
-            })
-        })
     }
 
+
+/**
+ * Toggles whether or not the users message icon and dropdown is shown
+* @param {event} e - The submit event triggered by clicking the messages icon.
+ */
     handleClick(e){
         if (!this.shown){
             e.currentTarget.lastElementChild.classList.add("shown")
@@ -39,11 +49,10 @@ export default class Messages extends React.Component{
             this.shown = false
         }
     }
-
+/**
+* Renders the message icon and dropdown
+*/
     render(){
-        
-        let notif = this.props.unread || ""
-    
         return(
             <div>
                 <MessagePanelContainer />
